4d4b7f5305569dced1ae30636c208b11418e98cd
tele-studio <github@tele.studio>
2022-11-15 12:08:53 +0000

    1.0

--- tele-install.sh
+++ tele-install.sh
@@ -1,12 +1,12 @@
 USER="tele"
-PASSWD="tele"
+PASSWD="telepwd"
 # DB_NAME="tele_db"
 
 # Create a DB user
-psql -c "create user tele with encrypted password 'telepwd';"
-psql -c "ALTER ROLE tele WITH SUPERUSER;"
-psql -c "GRANT ALL PRIVILEGES ON DATABASE template0 TO tele;"
-psql -c "GRANT ALL PRIVILEGES ON DATABASE template1 TO tele;"
+psql -c "create user $USER with encrypted password '$PASSWD';"
+psql -c "ALTER ROLE  $USER WITH SUPERUSER;"
+psql -c "GRANT ALL PRIVILEGES ON DATABASE template0 TO $USER;"
+psql -c "GRANT ALL PRIVILEGES ON DATABASE template1 TO $USER;"
 
 # Create a database
 # psql -c "CREATE DATABASE $DB_NAME WITH OWNER $USER;"
@@ -22,4 +22,3 @@ python -m pip install --upgrade pip
 
 pip3 install wheel
 pip3 install -U -r tele/requirements.txt
-
